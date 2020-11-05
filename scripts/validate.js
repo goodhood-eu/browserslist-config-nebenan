1218219be39096fe2b7e192e6ca8044f8d16c64a
Peter Haßler <pet.hassler@gmail.com>
2020-11-05 18:28:36 +0100

    Add output to validate script and add linting

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,7 +3,9 @@ const config = require('../index');
 
 try {
   browserslist(config);
+  console.log('Browserslist config is valid');
 } catch (e) {
+  console.error('Browserslist config is invalid');
   console.error(e);
   process.exit(1);
 }
